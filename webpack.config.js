111374c01fab4a52ca71d77ccda56c1eb98c3a6b
Anthony White <brancoantonio18@gmail.com>
2018-05-18 15:32:31 +0100

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,16 @@ var path = require('path');
 
 module.exports = {
     //Creates a bundle out of app.js with babel-polyfill appended
-    entry: [/*"babel-polyfill", */(__dirname + '/src/assets/js/app.js')],
+    entry: [ /*"babel-polyfill", */ (__dirname + '/src/assets/js/app.js')],
     output: {
         path: path.join(__dirname, '/src/Assets'),
         filename: 'bundle.js'
     },
     module: {
         rules: [{
-                // test: /\.js$/,
-                // exclude: /node_modules/,
-                // use: [{
-                //     loader: 'babel-loader',
-                //     query: {
-                //         presets: ["react"]
-                //     }
-                // }],
-            }
-        ]
+            test: /\.jsx?$/,
+            use: 'babel-loader',
+            exclude: /node_modules/,
+        }]
     }
 };
