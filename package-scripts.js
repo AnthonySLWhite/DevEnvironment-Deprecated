4dee89fac77e2a28dba068624bc669720584049d
Anthony White <brancoantonio18@gmail.com>
2018-12-19 10:54:38 +0000

    Temp commit
    Changing between laptops

--- package-scripts.js
+++ package-scripts.js
@@ -1,78 +1,165 @@
 const npsUtils = require('nps-utils');
 module.exports = {
   scripts: {
-    test: npsUtils.concurrent.nps('test2'),
-    test2: 'echo thats 1',
-    test3: 'echo thats 2',
-    herokuPrebuild: npsUtils.concurrent.nps('parcel:build'),
-    prepack: npsUtils.concurrent.nps('parcel:build'),
-    default: npsUtils.concurrent.nps('nodemon-prod'),
+    // prettier: 'prettier --config ./.prettierrc.js src/app.js --write',
+    // eslint: 'eslint --fix src/app.js',
+    // test: 'eslint --print-config .eslintrc.json | eslint-config-prettier-check',
+    default: {
+      // NPM START for deployment
+      script: 'nps nodemon.node.production',
+      hiddenFromHelp: true,
+    },
     /*
     <==========================================>
     <                  Node.js                 >
     <==========================================>
     */
-    codeN: {
-      default: npsUtils.concurrent.nps('parcel', 'nodemon-dev'),
-      build: npsUtils.series.nps('parcel:build', 'nodemon-prod')
+    node: {
+      // Development mode
+      default: {
+        script: npsUtils.concurrent.nps('parcel.node', 'nodemon-dev'),
+        description: 'Development mode for Node.js',
+      },
+      // Builds
+      build: {
+        default: {
+          script: npsUtils.concurrent.nps('parcel:build'),
+          description: 'Build Node.js project',
+        },
+        run: {
+          script: npsUtils.series.nps('parcel:build', 'nodemon-prod'),
+          description: 'Build Node.js project and run server',
+        },
+      },
+      // Debug mode
+      debug: {
+        script: npsUtils.concurrent.nps('parcel.node', 'nodemon.node.debug'),
+        description: 'Debug Node.js project',
+      },
     },
-    buildN: npsUtils.concurrent.nps('parcel:build'),
     /*
     <==========================================>
     <                    DOM                   >
     <==========================================>
     */
-    codeF: {
-      default: 'npm-run-all --parallel nodemon-frontend sass parcel-frontend',
-      build:
-        'npm-run-all --sequential sass:build prefixer parcel-frontend:build codeN:build'
+    dom: {
+      // Development mode
+      default: {
+        script: npsUtils.concurrent.nps(
+          'sass',
+          'parcel-frontend',
+          'nodemon-frontend',
+        ),
+        description: 'Development mode for DOM',
+      },
+      // Builds
+      build: {
+        default: {
+          script: npsUtils.series.nps(
+            'sass:build',
+            'prefixer',
+            'parcel-frontend:build',
+          ),
+          description: 'Build DOM project',
+        },
+        run: {
+          script: npsUtils.series.nps(
+            'sass:build',
+            'prefixer',
+            'parcel-frontend:build',
+            'nodemon-frontend',
+          ),
+          description: 'Build DOM project and run server',
+        },
+      },
     },
-    buildF:
-      'npm-run-all --sequential sass:build prefixer parcel-frontend:build',
     /*
     <==========================================>
-    <                 Nodemon-Node             >
+    <                   Nodemon                >
     <==========================================>
     */
-    nodemonDev: 'cross-env NODE_ENV=development nodemon ./dist/app.js',
-    nodemonProd: 'cross-env NODE_ENV=production nodemon ./prod/app.js',
-    debug: 'nodemon --inspect-brk ./dist/app.js',
-    /*
-    <==========================================>
-    <                 Nodemon-DOM              >
-    <==========================================>
-    */
-    nodemonFrontend:
-      'cross-env NODE_ENV=production FrontEnd=true nodemon ./prod/app.js',
-    /*
-    <==========================================>
-    <                 DOM-Scripts              >
-    <==========================================>
-    */
-    sass: {
-      default: 'npm-run-all --sequential sass:build sass:watch',
-      watch:
-        'node-sass --watch -r --include-path "public/src/assets/css" -o "public/src/assets/css" --source-map true "public/src/assets/css/"',
-      build:
-        'node-sass -o "public/src/assets/css" --source-map true "public/src/assets/css"'
+    nodemon: {
+      node: {
+        // Default as development
+        default: {
+          script: 'cross-env NODE_ENV=development nodemon ./dist/app.js',
+          hiddenFromHelp: true,
+        },
+        production: {
+          script: 'cross-env NODE_ENV=production nodemon ./prod/app.js',
+          hiddenFromHelp: true,
+        },
+        debug: {
+          script: 'nodemon --inspect-brk ./dist/app.js',
+          hiddenFromHelp: true,
+        },
+      },
+
+      dom: {
+        default: {
+          script:
+            'cross-env NODE_ENV=production FrontEnd=true nodemon ./prod/app.js',
+          hiddenFromHelp: true,
+        },
+      },
     },
-    prefixer:
-      'postcss -r --config postcss.build.config.js "public/src/assets/css/style.css" ',
     /*
     <==========================================>
-    <                Parcel-Scripts            >
+    <                    Parcel                >
     <==========================================>
     */
     parcel: {
-      default: 'parcel watch src/app.js --public-url ./ --target node',
-      build:
-        'parcel build src/app.js --public-url ./ --out-dir prod --target node'
+      node: {
+        default: {
+          script:
+            'cross-env NODE_ENV=development parcel watch src/app.js --public-url ./ --target node',
+          hiddenFromHelp: true,
+        },
+        build: {
+          script:
+            'cross-env NODE_ENV=production parcel build src/app.js --public-url ./ --out-dir prod --target node',
+          hiddenFromHelp: true,
+        },
+      },
+      dom: {
+        default: {
+          script:
+            'cross-env NODE_ENV=development parcel watch public/src/index.html --out-dir public/dist --target browser',
+          hiddenFromHelp: true,
+        },
+        build: {
+          script:
+            'cross-env NODE_ENV=production parcel build public/src/index.html --out-dir public/prod --target browser',
+          hiddenFromHelp: true,
+        },
+      },
     },
-    parcelFrontend: {
-      default:
-        'cross-env NODE_ENV=development parcel watch public/src/index.html --out-dir public/dist --target browser',
-      build:
-        'cross-env NODE_ENV=production parcel build public/src/index.html --out-dir public/prod --target browser'
-    }
-  }
+    /*
+    <==========================================>
+    <                  Sass/CSS                >
+    <==========================================>
+    */
+    sass: {
+      default: {
+        // Initial build of sass then watch for file changes
+        script: npsUtils.series.nps('sass.build', 'sass.watch'),
+        hiddenFromHelp: true,
+      },
+      build: {
+        script:
+          'node-sass -o "public/src/assets/css" --source-map true "public/src/assets/css"',
+        hiddenFromHelp: true,
+      },
+      watch: {
+        script:
+          'node-sass --watch -r --include-path "public/src/assets/css" -o "public/src/assets/css" --source-map true "public/src/assets/css/"',
+        hiddenFromHelp: true,
+      },
+      prefixer: {
+        script:
+          'postcss -r --config postcss.build.config.js "public/src/assets/css/style.css"',
+        hiddenFromHelp: true,
+      },
+    },
+  },
 };
--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import '@babel/polyfill';
 import 'dotenv/config';
-console.log(`This is my ${process.env.MY_DATABASE_PASSWORD} `);
+console.log(`This is my ${process.env.MY_DATABASE_PASSWORD}`);
 
 import './server/server';
+const x = [5, 3, 5, 'Hello', 4, 8, 4, 6];
+
+const y = { 1: 1 };
