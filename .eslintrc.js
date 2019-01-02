45b2f2aa64792a843c8e871e47a70d0e1b0be198
Anthony White <brancoantonio18@gmail.com>
2019-01-02 13:29:20 +0000

    Major Update
    Overall Updated made previously in the Node-React-API project
    
    Changes made:
    Refactor :
    - Exported npm scripts using NPS
    - Updated EsLint
    - Updated Babel
    - Added global Readme with some changes!
    -  Added License
    - Updated Git ignore

---
+++ .eslintrc.js
@@ -0,0 +1,81 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: false,
+    node: true,
+    mocha: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      impliedStrict: true,
+    },
+  },
+  extends: [
+    'google',
+    'problems',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+  ],
+
+  plugins: ['fp', 'prettier'],
+  rules: {
+    /*
+    <==========================================>
+    <                 EsLint                   >
+    <==========================================>
+    */
+    'no-console': 1,
+    'prefer-const': 1,
+    'prefer-arrow-callback': 1,
+    'no-unused-vars': 0,
+    'no-var': 2,
+    'no-buffer-constructor': 0,
+    strict: 2,
+    'switch-colon-spacing': 0,
+    'consistent-return': 1,
+    'comma-dangle': [0, 'always-multiline'],
+    /*
+    <==========================================>
+    <          Functional programming          >
+    <==========================================>
+    */
+    'fp/no-arguments': 2,
+    'fp/no-class': 0,
+    'fp/no-delete': 2,
+    'fp/no-events': 2,
+    'fp/no-get-set': 2,
+    'fp/no-let': 0,
+    'fp/no-loops': 1,
+    'fp/no-nil': 0,
+    'fp/no-proxy': 2,
+    'fp/no-rest-parameters': 2,
+    'fp/no-throw': 2,
+    'fp/no-valueof-field': 2,
+    'fp/no-mutation': 0,
+    'fp/no-mutating-assign': 1,
+    'fp/no-mutating-methods': 0,
+    'fp/no-this': 1,
+    'fp/no-unused-expression': 0,
+    /*
+    <==========================================>
+    <                  Others                  >
+    <==========================================>
+    */
+    'prettier/prettier': [
+      2,
+      {
+        usePrettierrc: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 65,
+        eslintIntegration: true,
+      },
+    ],
+  },
+};
